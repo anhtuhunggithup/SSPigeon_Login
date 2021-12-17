573a44aa59daac1142178d8494f393c3a6ff3515
anhtuhunggithup <anhtuhungdeveloper@gmail.com>
2021-12-18 00:53:40 +0700

    SS Pigeon

---
+++ app/src/main/java/app/com/sspigeon/Profile.java
@@ -0,0 +1,43 @@
+package app.com.sspigeon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.AndroidException;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends Activity {
+
+    AutoCompleteTextView textMonth, textGender;
+    String[] months, genders;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+        Begin();
+        CreateGender();
+        CreateMonth();
+    }
+
+    private void Begin(){
+        textMonth = (AutoCompleteTextView) findViewById(R.id.Month);
+        textGender = (AutoCompleteTextView) findViewById(R.id.Gender);
+    }
+
+    private void CreateMonth(){
+        months = getResources().getStringArray(R.array.Months);
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(Profile.this, R.layout.support_simple_spinner_dropdown_item, months);
+        textMonth.setDropDownHeight(400);
+        textMonth.setAdapter(monthAdapter);
+    }
+
+    private void CreateGender(){
+        genders = getResources().getStringArray(R.array.Genders);
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(Profile.this, R.layout.support_simple_spinner_dropdown_item, genders);
+        textGender.setAdapter(monthAdapter);
+    }
+}
